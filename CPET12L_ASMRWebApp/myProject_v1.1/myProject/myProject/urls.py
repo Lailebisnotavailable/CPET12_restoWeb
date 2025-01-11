0619be2cb256f46642c32427842fd67f819fad12
K4TR13 <168205138+K4TR13@users.noreply.github.com>
2025-01-11 12:22:28 +0800

    Delete CPET12L_ASMRWebApp/myProject_v1.1 directory

--- CPET12L_ASMRWebApp/myProject_v1.1/myProject/myProject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('', include('GenkaiZoneRailImpact.urls')),
-    path('admin/', admin.site.urls),
-]
